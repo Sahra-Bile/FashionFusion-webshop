7c6020da260aef53c9552a811b495f918c29d17d
Sahra <sahra.bile134@gmail.com>
2023-10-02 20:52:44 +0200

    add the child thema a functions.php file

--- themes/storefrontChild/functions.php
+++ themes/storefrontChild/functions.php
@@ -5,3 +5,20 @@ function enqueue_child_theme_styles()
     // Lägg till eventuella anpassade CSS-filer eller stilar här
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_theme_styles');
+
+function create_custom_post_type()
+{
+    register_post_type(
+        'butik',
+        array(
+            'labels' => array(
+                'name' => __('Butiker'),
+                'singular_name' => __('Butik')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title', 'editor'),
+        )
+    );
+}
+add_action('init', 'create_custom_post_type');
