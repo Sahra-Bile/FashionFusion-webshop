dfa62c8f6afd896b1474f81a9952fcd3c61f253e
Sahra <sahra.bile134@gmail.com>
2023-10-03 00:03:44 +0200

    Add a  list for all stores

---
+++ themes/storefrontChild/template-butiker.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Template Name: Butiker
+ */
+get_header();
+
+$args = array(
+    'post_type' => 'butik',
+    // Uppdatera till rätt posttypsnamn
+    'posts_per_page' => -1,
+);
+
+$query = new WP_Query($args);
+
+if ($query->have_posts()):
+    while ($query->have_posts()):
+        $query->the_post();
+        echo '<div class="butik">';
+        echo '<h2>' . get_the_title() . '</h2>';
+        echo '<p>Adress: ' . get_field('adress') . '</p>';
+        echo '<p>Postnumret: ' . get_field('postnummer') . '</p>';
+        echo '<p>Postadress: ' . get_field('postadress') . '</p>';
+        echo '<p>Öppettider: ' . get_field('oppettider') . '</p>';
+        echo '<p>Öppettider lördagar: ' . get_field('oppettider_lordagar') . '</p>';
+        echo '<p>Öppettider söndagar: ' . get_field('oppettider_sondagar') . '</p>';
+        echo '<p> Avvikande Öppettider: ' . get_field('avvikande_oppettider') . '</p>';
+        // Lägg till andra fält här
+        echo '</div>';
+    endwhile;
+endif;
+
+wp_reset_postdata();
+
+get_footer();
+?>
